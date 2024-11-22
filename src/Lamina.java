6b4b2eea7e412ccebffc6235a92ba405e2fd54d0
icatoira0 <icatoira0@iescotarelo.org>
2024-11-22 20:28:30 +0100

    Diseño de la calculadora, botones funcionales, funcionalidad general realizada

---
+++ src/Lamina.java
@@ -0,0 +1,159 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Lamina extends JPanel implements ActionListener, KeyListener {
+
+    JTextField texto = new JTextField();
+
+    private double num1 = 0;
+    private double num2 = 0;
+    private String operador = "";
+
+    public Lamina() {
+        setLayout(new BorderLayout());
+        texto.setHorizontalAlignment(SwingConstants.RIGHT);
+        texto.setFont(new Font("Arial", Font.BOLD, 50));
+        texto.setEditable(false);
+        add(texto, BorderLayout.NORTH);
+        JPanel botones = crearPanel();
+        add(botones);
+        texto.addKeyListener(this);
+        
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    private JPanel crearPanel() {
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new GridLayout(5, 4, 6, 6));
+        String[] botones = { "7", "8", "9", "CE", "4", "5", "6", "+", "1", "2", "3", "-", "0", "*", "/", "=", "." };
+        for (String boton : botones) {
+            JButton button = new JButton(boton);
+            button.setFont(new Font("Arial", Font.BOLD, 45));
+            panelBotones.add(button);
+            button.addActionListener(this);
+            if (!boton.equals("=")) {
+                button.setBackground(new Color(242, 242, 242));
+            } else {
+                button.setBackground(new Color(255, 108, 0));
+            }
+        }
+        panelBotones.setBackground(new Color(188, 188, 188));
+        return panelBotones;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            String pulsado = e.getActionCommand();
+            if ("+-*/".contains(pulsado) && !texto.getText().isEmpty()) {
+                if (num1 == 0) {
+                    num1 = Double.parseDouble(texto.getText());
+                    operador = pulsado;
+                    texto.setText("");
+                } else {
+                    num2 = Double.parseDouble(texto.getText());
+                    num1 = calcular(num1, num2, operador);
+                    operador = "";
+                    num2 = 0;
+                    texto.setText("");
+                }
+            } else if ("0123456789.".contains(pulsado)) {
+                texto.setText(texto.getText() + pulsado);
+            } else if (pulsado.equals("=")) {
+                num2 = Double.parseDouble(texto.getText());
+                texto.setText(calcular(num1, num2, operador) + "");
+                num1 = 0;
+                num2 = 0;
+                operador = "";
+            } else {
+                texto.setText("");
+                num1 = 0;
+                num2 = 0;
+            }
+        } catch (NullPointerException | NumberFormatException f) {
+            f.printStackTrace();
+            num1 = 0;
+            num2 = 0;
+            texto.setText("");
+        }
+    }
+
+    private static double calcular(double num1, double num2, String operador) {
+        double resultado = 0;
+        switch (operador) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+        }
+        return resultado;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        try {
+            String pulsado = e.get;
+            if ("+-*/".contains(pulsado) && !texto.getText().isEmpty()) {
+                if (num1 == 0) {
+                    num1 = Double.parseDouble(texto.getText());
+                    operador = pulsado;
+                    texto.setText("");
+                } else {
+                    num2 = Double.parseDouble(texto.getText());
+                    num1 = calcular(num1, num2, operador);
+                    operador = "";
+                    num2 = 0;
+                    texto.setText("");
+                }
+            } else if ("0123456789.".contains(pulsado)) {
+                texto.setText(texto.getText() + pulsado);
+            } else if (pulsado.equals("=")) {
+                num2 = Double.parseDouble(texto.getText());
+                texto.setText(calcular(num1, num2, operador) + "");
+                num1 = 0;
+                num2 = 0;
+                operador = "";
+            } else {
+                texto.setText("");
+                num1 = 0;
+                num2 = 0;
+            }
+        } catch (NullPointerException | NumberFormatException f) {
+            f.printStackTrace();
+            num1 = 0;
+            num2 = 0;
+            texto.setText("");
+        }
+    }
+}
---
+++ src/Marco.java
@@ -0,0 +1,36 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ * Clase que representa el marco donde esta la calculadora
+ * y que se encarga de las operaciónes
+ * 
+ * @author Ismael Catoira Rial
+ */
+public class Marco extends JFrame{
+    
+    /**
+     * Constructor paramétrizado
+     */
+    public Marco() {
+        Toolkit pantalla = Toolkit.getDefaultToolkit();
+        Dimension tamanoPant = pantalla.getScreenSize();
+        int alturaPant = tamanoPant.height;
+        int anchoPant = tamanoPant.width;
+
+        setTitle("Calculadora");
+        setSize(anchoPant / 2, 600);
+        setLocation(anchoPant / 4, alturaPant / 4);
+
+        Lamina lamina = new Lamina();
+        add(lamina);
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new Marco();
+    }
+}
